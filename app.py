b14cc837ea9c0c93eff9a97a00f1f1417df2219b
GHTGT <2574159605@qq.com>
2023-07-14 12:49:24 +0800

    完成了账号登入与注册

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template, request, make_response
+from flask_sqlalchemy import SQLAlchemy
 import JiamiChar
 import time
-# import SQLCAOZUO
-
+import json
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1:3306/flasksql?charset=utf8mb4'
+db = SQLAlchemy(app)
 nowpage = ''
 cookies = ''
-login = 0  # 0=未登入 1=登入成功 2=账号或密码错误 3=账号不存在
+login = 0  # 0=未登入 1=登入成功 2=账号或密码错误 3=注册成功，请登入
+reg = 0  # 0=None 1=账号已存在
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), nullable=False)
+    # public_id = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    # admin = db.Column(db.Boolean)
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
 @app.route('/index')
@@ -32,17 +52,33 @@ def download_page():
 def chatroom_login():
     global login
     nowpage = 'chatroom'
-
-    if login != 1:
+    if request.method == "POST":
+        a = request.values
+        login_user = User.query.filter_by(username=a['username'], password=a['password'])
+        if login_user.first() is not None:
+            # 登入成功
+            return "欢迎用户：" + login_user[0].username + "登入访问"
+        else:
+            return render_template('chatroom_login.html', nowpage=nowpage, login=2)
+    else:
         return render_template('chatroom_login.html', nowpage=nowpage, login=login)
-    if login == 1:
-        return app.redirect('/chatroom')
-    return render_template('chatroom_login.html', nowpage=nowpage, login=login)
 
 
-@app.route("/chatroom_registered", methods=['POST'])
+@app.route("/chatroom_registered", methods=['POST', 'GET'])
 def chatroom_reg():
-    pass
+    if request.method == "POST":
+        a = request.values
+        reg_user = User.query.filter_by(username=a['username'])
+        if reg_user.first() is not None:
+            return render_template('chatroom_registered.html', reg=1)
+        else:
+            reg_user_mysql = User(username=a['username'], password=a['password'])
+            db.session.add(reg_user_mysql)
+            db.session.commit()
+            return app.redirect('chatroom_login')
+            # return render_template('chatroom_login.html', nowpage='chatroom', login=3)
+    else:
+        return render_template('chatroom_registered.html', nowpage=nowpage, reg=0)
 
 
 @app.route('/chatroom', methods=['POST', 'GET'])
